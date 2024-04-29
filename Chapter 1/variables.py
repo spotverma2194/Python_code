c9898084911039a1bed9371acb44b40a5c5a87ae
spotverma2194 <deepakverm0522@gmail.com>
2024-04-29 14:49:09 +0530

    Variable in Python

---
+++ Chapter 1/variables.py
@@ -0,0 +1,12 @@
+number1 =2
+print(number1)
+number1 =4
+print(number1)
+#string,number
+name= "Deepak"
+print(name)
+name =123 #dynamic programming
+print(name)
+useronename= " Rohit"
+user_one_name="Rohit " #snake case writing
+userOneName="Mohit" #camel case writing
